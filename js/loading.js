de6d12e3250e26f56f4c7d96c42278269c4f1119
LapminerYT <96932774+LapminerYT@users.noreply.github.com>
2023-06-12 18:16:45 +0700

    Create loading.js

---
+++ js/loading.js
@@ -0,0 +1,24 @@
+// =====================
+
+//        LOADING
+
+// =====================
+
+
+
+var timeLimit = 5000;
+
+window.addEventListener("load", function() {
+
+	window.location.href = "/web/index.html";
+});
+
+
+
+var errorTimer = setTimeout(function() {
+
+	document.getElementById("loading").style.display = "none";
+
+	document.getElementById("error-message").style.display = "block";
+
+}, timeLimit);
